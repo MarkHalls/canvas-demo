9da78b3f996dc5c531e7ff7643975e883885cae8
Mark Halls <mark.halls@gmail.com>
2021-01-23 16:22:57 -0700

    Respect bounds of canvas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
 import "normalize.css";
 
-import * as game from "@mark-halls/game-of-life-data";
+import {
+  create,
+  createSeeded,
+  next,
+  boundedNext,
+} from "@mark-halls/game-of-life-data";
 
 const pixelSize = 8;
 
 let gameCoords = [];
 
+// const create = (game) => {
+//   const random = Math.random().toString();
+//   return createSeeded(game, random);
+// };
+// exports.create = create;
+
+// const createSeeded = (game, seed) => {
+//   const rng = seedrandom_1.default.alea(seed);
+
+//   console.log(game);
+//   if (!game.density) {
+//       game.density = 0;
+//   }
+//   return naiveRandomCoords(game, rng);
+// };
+
 const draw = () => {
   const canvas = document.getElementById("canvas");
   canvas.width = document.body.clientWidth;
   canvas.height = document.body.clientHeight + 225;
-  gameCoords = game.create({
-    xMin: 0,
-    xMax: canvas.width / pixelSize,
-    yMin: 0,
-    yMax: canvas.height / pixelSize,
-    density: 0.2,
-  });
+
+  const testing = create({ xMin: 0, xMax: 2, yMin: 0, yMax: 2 });
+  // console.log(testing);
+
+  gameCoords = createSeeded(
+    {
+      xMin: 0,
+      xMax: canvas.width / pixelSize,
+      yMin: 0,
+      yMax: canvas.height / pixelSize,
+      density: 0.2,
+    },
+    "seed"
+  );
 
   console.log(gameCoords);
 
@@ -25,7 +53,7 @@ const draw = () => {
 
     setInterval(() => {
       gameCoords = drawGame(ctx, gameCoords, canvas);
-    }, 10);
+    }, 300);
   }
 };
 
@@ -36,7 +64,15 @@ const drawGame = (ctx, gameCoords, canvas) => {
     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
     ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
   });
-  const newCoords = game.next(gameCoords);
+  const newCoords = boundedNext(
+    {
+      xMin: 0,
+      xMax: canvas.width / pixelSize,
+      yMin: 0,
+      yMax: canvas.height / pixelSize,
+    },
+    gameCoords
+  );
   //   console.log(newCoords);
   return newCoords;
 };
